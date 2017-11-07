4591278755ab3b88ae733e3f751f42b26f8cffe0
scmbld <emaild@tutanota.com>
2017-11-08 00:20:41 +0530

    ensble spring security

--- src/main/resources/static/js/devopsbuddy.js
+++ src/main/resources/static/js/devopsbuddy.js
@@ -1,12 +1,23 @@
-$(document).ready(main);
+$( document ).ready( main );
 
-function main(){
-	$('.btn-collapse').click(function(e)
-	{
-		e.preventDefault();
-		var $this=$(this);
-		var $collapse =$this.closest('.collapse-group').find('.collapse');
-		$collapse.collapse('toggle');
-	}	);
-			
+
+
+function main() {
+
+
+
+    $('.btn-collapse').click(function(e) {
+
+        e.preventDefault();
+
+        var $this = $(this);
+
+        var $collapse = $this.closest('.collapse-group').find('.collapse');
+
+        $collapse.collapse('toggle');
+
+    });
+
+
+
 }
