1efb43f4155cea6fd7e126fbc362ae9a1322be69
scmbld <emaild@tutanota.com>
2017-11-12 01:48:57 +0530

    passeor d enc

--- src/main/java/com/devopsbuddy/backend/persistence/repositories/UserRepository.java
+++ src/main/java/com/devopsbuddy/backend/persistence/repositories/UserRepository.java
@@ -3,18 +3,40 @@
  */
 package com.devopsbuddy.backend.persistence.repositories;
 
+import org.jboss.logging.Param;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.devopsbuddy.backend.persistence.domain.backend.User;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author scmbld on 11-Nov-2017
  *
  */
+ 
+ 
+ 
 @Repository
-public interface UserRepository  extends CrudRepository<User, Long>{
-	
-	
+@Transactional(readOnly = true)
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    /**
+     * Returns a User given a username or null if not found.
+     * @param username The username
+     * @return a User given a username or null if not found.
+     */
+    User findByUsername(String username);
+
+    /**
+     * Returns a User for the given email or null if none was found.
+     * @param email The user's email
+     * @return a User for the given email or null if none was found.
+     */
+    User findByEmail(String email);
+
+   
 
 }
--- src/main/java/com/devopsbuddy/configs/SecurityConfig.java
+++ src/main/java/com/devopsbuddy/configs/SecurityConfig.java
@@ -1,13 +1,18 @@
 package com.devopsbuddy.configs;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import com.devopsbuddy.backend.service.UserSecurityService;
+
+import java.security.SecureRandom;
 import java.util.Arrays;
 import java.util.List;
 
@@ -18,8 +23,27 @@ import java.util.List;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	
+
+
+    @Autowired
+
+    private UserSecurityService userSecurityService;
+    
+    
     @Autowired
     private Environment env;
+    
+    private static final String SALT= "eerererwerwer234234234";
+    
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+
+        return new BCryptPasswordEncoder(12, new SecureRandom(SALT.getBytes()));
+
+    }
+
+
 
     /** Public URLs. */
     private static final String[] PUBLIC_MATCHERS = {
@@ -57,8 +81,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth
-                .inMemoryAuthentication()
-                .withUser("user").password("password")
-                .roles("USER");
+        
+        	.userDetailsService(userSecurityService)
+        	.passwordEncoder(passwordEncoder());
     }
 }
